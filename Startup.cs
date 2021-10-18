b04e41839d0f94303dcbe2a6f27afa23eeb43665
calaw1110 <calaw1110@gmail.com>
2021-10-18 22:42:02 +0800

    使用 asp.net core identity 開發會員註冊api 調整startup功能 新增會員註冊 controller api

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using CCMSAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -29,6 +30,7 @@ namespace CCMSAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -38,20 +40,52 @@ namespace CCMSAPI
             services.AddDbContext<AuthenticationContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("CCMSConnection")));
 
-            services.AddDefaultIdentity<User>()
+            services.AddDefaultIdentity<ApplicationUser>()
                 .AddEntityFrameworkStores<AuthenticationContext>();
+
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                //測試用 關閉密碼驗證的條件 
+                options.Password.RequiredLength = 4;//密碼最短長度
+                options.Password.RequireDigit = false; //至少有一個數字
+                options.Password.RequireNonAlphanumeric = false; //至少一個英文
+                options.Password.RequireLowercase = false; //至少一個小寫英文
+                options.Password.RequireUppercase = false; //至少一個大寫英文
+            });
+
+            //新增跨站請求服務
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //某版.net core問題 狀態code 204 會被IIS當成錯誤處理
+            app.Use(async (ctx, next) =>
+            {
+                await next();
+                if (ctx.Response.StatusCode == 204)
+                {
+                    ctx.Response.ContentLength = 0;
+                }
+            });
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CCMSAPI v1"));
             }
-            
+
+            //跨站請求開放
+            app.UseCors(builder =>
+            builder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            );
             app.UseHttpsRedirection();
 
             app.UseRouting();
